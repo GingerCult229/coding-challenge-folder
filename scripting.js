291e6c4d6b7af5190795393015ad602cdaf721f0
GingerCult229 <37348008+GingerCult229@users.noreply.github.com>
2021-05-23 20:36:48 +0100

    Add files via upload

---
+++ scripting.js
@@ -0,0 +1,26 @@
+
+
+
+function setContent(index)
+{
+  if (index == 1) {
+      document.getElementById('title').innerHTML = "Tokyo Ghoul :re";
+      document.getElementById('jp-title').innerHTML = "(東京喰種 :re)";
+      document.getElementById('image').src = "https://maserpatrol.files.wordpress.com/2018/03/tokyo-ghoul-re.jpg";
+      document.getElementById('body').innerHTML = "Two years have passed since the CCG's raid on Anteiku. Although the atmosphere in Tokyo has changed drastically due to the increased influence of the CCG, ghouls continue to pose a problem as they have begun taking caution, especially the terrorist organization Aogiri Tree, who acknowledge the CCG's growing threat to their existence.</br></br>The creation of a special team, known as the Quinx Squad, may provide the CCG with the push they need to exterminate Tokyo's unwanted residents. As humans who have undergone surgery in order to make use of the special abilities of ghouls, they participate in operations to eradicate the dangerous creatures. The leader of this group, Haise Sasaki, is a half-ghoul, half-human who has been trained by famed special class investigator, Kishou Arima. However, there's more to this young man than meets the eye, as unknown memories claw at his mind, slowly reminding him of the person he used to be."
+  }
+
+  else if (index == 2) {
+      document.getElementById('title').innerHTML = "Demon Slayer";
+      document.getElementById('jp-title').innerHTML = "(鬼滅の刃)";
+      document.getElementById('image').src = "https://m.media-amazon.com/images/I/71Tg-bQcoSL._AC_SL1200_.jpg";
+      document.getElementById('body').innerHTML = "Ever since the death of his father, the burden of supporting the family has fallen upon Tanjirou Kamado's shoulders. Though living impoverished on a remote mountain, the Kamado family are able to enjoy a relatively peaceful and happy life. One day, Tanjirou decides to go down to the local village to make a little money selling charcoal. On his way back, night falls, forcing Tanjirou to take shelter in the house of a strange man, who warns him of the existence of flesh-eating demons that lurk in the woods at night.</br></br>When he finally arrives back home the next day, he is met with a horrifying sight—his whole family has been slaughtered. Worse still, the sole survivor is his sister Nezuko, who has been turned into a bloodthirsty demon. Consumed by rage and hatred, Tanjirou swears to avenge his family and stay by his only remaining sibling. Alongside the mysterious group calling themselves the Demon Slayer Corps, Tanjirou will do whatever it takes to slay the demons and protect the remnants of his beloved sister's humanity.";
+  }
+
+  else if (index == 3){
+      document.getElementById('title').innerHTML = "Fire Force";
+      document.getElementById('jp-title').innerHTML = "(炎炎ノ消防隊)";
+      document.getElementById('image').src = "https://m.media-amazon.com/images/M/MV5BY2IyMDA0NGEtZjIyOS00NjU0LThlOTctODA0OTZmMDU2ZTMxXkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_.jpg";
+      document.getElementById('body').innerHTML = "Spontaneous Human Combustion: a chaotic phenomenon that has plagued humanity for years, randomly transforming ordinary people into flaming, violent creatures known as Infernals. While Infernals make up the first-generation accounts of Human Combustion, the second and third generations became known as pyrokinetics—people gifted with the ability to manipulate and control their flames while remaining human. To combat the Infernal threat and discover the cause, the Tokyo Armed Forces, Fire Defense Agency, and Holy Church of Sol produced their answer: the Special Fire Force.</br></br>Young and eager third-generation pyrokinetic Shinra Kusakabe, nicknamed Devil's Footprints for his explosive ability to ignite his feet at will, becomes a member of the lively Special Fire Force Company 8. Upholding the brigade's duty to extinguish the blazing Infernals and lay their souls to rest, Shinra is determined to become a hero who will save the lives of those threatened by the flame terror.</br></br>However, this is not the hero's game Shinra imagined. The Fire Force is a fractured mess of feuding brigades, abnormal Infernal sightings are increasing all over Tokyo, and a shadowy group is claiming to have answers to the strange fire that caused the death of Shinra's family 12 years ago. Faced with many obstacles within and outside the Fire Force, Shinra fights to uncover the truth behind the burning mysteries that have kept him in the dark.";
+  }
+}
